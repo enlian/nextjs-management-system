b50cf537604a47ccb075140d0669b0ec03e4c987
enlian <frejaenlian@gmail.com>
2024-09-28 00:27:32 +0900

    fix bug

--- app/ui/search.tsx
+++ app/ui/search.tsx
@@ -2,13 +2,16 @@
 
 import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
 import { useSearchParams, usePathname, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
 
 export default function Search({ placeholder }: { placeholder: string }) {
     const searchParams = useSearchParams();
     const pathname = usePathname();
     const { replace } = useRouter();
 
-    function handleSearch(term: string) {
+    //防抖,仅在用户停止输入后的特定时间（300 毫秒）运行代码
+    const handleSearch = useDebouncedCallback((term) => {
+        console.log(`Searching... ${term}`);
         const params = new URLSearchParams(searchParams);
         if (term) {
             params.set('query', term);
@@ -16,7 +19,9 @@ export default function Search({ placeholder }: { placeholder: string }) {
             params.delete('query');
         }
         replace(`${pathname}?${params.toString()}`);
-    }
+    }, 300);
+
+
   return (
     <div className="relative flex flex-1 flex-shrink-0">
       <label htmlFor="search" className="sr-only">
@@ -28,6 +33,7 @@ export default function Search({ placeholder }: { placeholder: string }) {
         onChange={(e) => {
             handleSearch(e.target.value);
         }}
+        defaultValue={searchParams.get('query')?.toString()}
       />
       <MagnifyingGlassIcon className="absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
     </div>
